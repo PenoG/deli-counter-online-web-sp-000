2345fc5ddc7e959f399ef1d50ebb3ed2028bdc23
Mason Phillips <mjphillips2000@gmail.com>
2020-10-07 07:39:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 def line(katz_deli)
   if katz_deli.length > 0
-    katz_deli.each 
+    katz_deli.each_with_index do |name, index|
+      return "The line is currently: #{index}. #{name}"
   puts "The line is currently empty."
 end
